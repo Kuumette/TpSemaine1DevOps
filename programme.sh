0adc33d17b0a4ff585b3c925429f62cc2335f25f
Kuumette <ballatoremanon@gmail.com>
2022-10-27 14:42:06 +0200

    bash test

--- programme.sh
+++ programme.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-fct_menu ()
-{
+# fct_menu ()
+# {
 echo
 echo "Choisissez une option [1-2]"
 echo
@@ -10,19 +9,22 @@ echo "2 : programme Calculatrice"
 echo "veuillez choisir :"
 
 read optionmenu
-	# case $optionmenu in
-	# "1" )
-	# 	echo "lancement du programme 1"
-	# 	python3 mainIp.py &;;
-	# "2" )
-	# 	echo "lancement du programme 2"
-	# 	python3 mainCal.py &;;
+	case $optionmenu in
+	"1" )
+		echo "lancement du programme 1"
+		python3 mainIp.py ;;
+		
+	"2" )
+		echo "lancement du programme 2"
+		python3 mainCal.py ;;
 	
-	# 	esac
-if[$optionmenu == 1]then
-	python3 mainIp.py 
-elif[$optionmenu == 2]then	
-	python3 mainCal.py
-
-}
-fct_menu
+	esac
+	# if [ $optionmenu == 1 ]; then
+	# 	#echo "1"
+	# 	python3 mainIp.py 
+	# elif [ $optionmenu == 2 ]; then
+	# 	echo "2"
+	# 	python3 mainCal.py
+	# fi
+# }
+# fct_menu
